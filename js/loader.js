a8279c58adbfa1b06192b6231def8ea258342f70
Efiria <bruno.doucet@viacesi.fr>
2020-07-20 09:58:46 +0200

    change loader

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,7 @@
 $.getJSON( "json/head.json", function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
- 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
+
+	$.each( data, function( key, val ) {
+		console.log(key + ' ' + val)
+	});
+
 });
