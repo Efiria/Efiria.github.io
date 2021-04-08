c14568172d268ed59100491871cceb0810bdbf29
Efiria <bruno.doucet@viacesi.fr>
2021-04-08 19:05:31 +0200

    add base project idle

---
+++ script/game.js
@@ -0,0 +1,55 @@
+const version = 'v0.1'
+const saveName = 'alchemySave'
+
+let total = 0
+
+function startGame() {
+    
+    document.getElementById("version").innerHTML = version;
+    
+    var save = getCookie(saveName)
+    if (save != null) {
+        document.getElementById("total").innerHTML = save;
+    } else {
+        document.getElementById("total").innerHTML = total;
+    }    
+}
+
+function updateTotal(newTotal) {
+    document.getElementById("total").innerHTML = newTotal;
+    save()
+}
+
+function count(value) {
+    total = total + value
+    updateTotal(total)
+}
+
+function save() {
+    var d = new Date();
+    d.setTime(d.getTime() + (100*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = saveName + "=" + total + ";" + expires + ";path=/";
+}
+
+function deleteSave() {
+    document.cookie = saveName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return null;
+  }
